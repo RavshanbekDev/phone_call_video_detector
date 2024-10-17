71a7e19b0fc5ca2df68f53da76c3fd0027a07b12
RavshanbekDev <ravshanbek98developer@gmail.com>
2024-10-17 10:20:13 +0500

    Made some changes

--- lib/phone_call_video_detector.dart
+++ lib/phone_call_video_detector.dart
@@ -1,9 +1,29 @@
 library phone_call_video_detector;
 
+import 'package:flutter/material.dart';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class CustomButton extends StatelessWidget {
+  final Function onPressed;
+  final Widget? child;
+  final TextStyle? style;
+
+  const CustomButton(
+      {super.key, required this.onPressed, this.child, this.style});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+        onPressed: () {
+          onPressed();
+        },
+        style: TextButton.styleFrom(
+          padding: const EdgeInsets.all(16.0),
+          backgroundColor: Colors.blue,
+          elevation: 9.0,
+          textStyle: const TextStyle(
+            fontSize: 20,
+          ),
+        ),
+        child: child ?? Container());
+  }
 }
-
